b858dfa46a84213f5c78a3a254337f123e04fa78
thanapat2402 <thanapat2402@gmail.com>
2022-07-25 15:48:37 +0700

    update web customer

--- src/app/components/web-customer/web-customer.component.ts
+++ src/app/components/web-customer/web-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TotalQueue } from 'src/app/models/customer';
 import { CustomerService } from 'src/app/service/customer.service';
 
 @Component({
@@ -7,7 +8,24 @@ import { CustomerService } from 'src/app/service/customer.service';
   styleUrls: ['./web-customer.component.css'],
 })
 export class WebCustomerComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  totalQueue: TotalQueue = {
+    AmountQueueA: '',
+    AmountQueueB: '',
+    AmountQueueC: '',
+    AmountQueueD: '',
+    CurrentqueueA: '',
+    CurrentqueueB: '',
+    CurrentqueueC: '',
+    CurrentqueueD: '',
+  };
+  constructor(private service: CustomerService) {}
+  getTotalQueue() {
+    this.service.getAmountQueue().subscribe((result) => {
+      console.log(result.data);
+      this.totalQueue = result.data;
+    });
+  }
+  ngOnInit(): void {
+    this.getTotalQueue();
+  }
 }
---
+++ src/app/models/customer.ts
@@ -0,0 +1,10 @@
+export interface TotalQueue {
+  AmountQueueA: String;
+  AmountQueueB: String;
+  AmountQueueC: String;
+  AmountQueueD: String;
+  CurrentqueueA: String;
+  CurrentqueueB: String;
+  CurrentqueueC: String;
+  CurrentqueueD: String;
+}
